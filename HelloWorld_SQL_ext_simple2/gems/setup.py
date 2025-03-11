be30492d3c9f6900f3857d2e392770d69982f902
kiran@prophecy.io <kiran@prophecy.io>
2025-03-11 05:32:49 +0000

    Cloning prophecy project main branch

---
+++ HelloWorld_SQL_ext_simple2/gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'HelloWorld_SQL',
+    version = '0.0.1',
+    packages = ['HelloWorld_SQL'],
+    package_dir = {'HelloWorld_SQL': '.'},
+    description = '',
+    install_requires = [],
+)
+
